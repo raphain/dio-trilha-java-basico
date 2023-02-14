90a4350f7459028dc4290da700319bcf68ab38bf
Raphain <raphain1@hotmail.com>
2023-02-14 00:20:04 -0300

    Adicionando tipos variaves

--- tipo-variaveis/src/TipoVariaveis.java
+++ tipo-variaveis/src/TipoVariaveis.java
@@ -1,5 +1,29 @@
+import java.util.Date;
+
 public class TipoVariaveis {
     public static void main(String[] args) {
-        System.out.println("Ola");
+        double salarioMinimo = 2500;
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        //short n2 = numeroNormal;
+
+        final double VALOR_PI = 3.14;
+
+        String nome = "GLEYSON";
+        int idade = 22;
+        double peso = 68.5;
+        char sexo = 'M';
+        boolean doadorOrgao = false;
+        Date dataNascimento = new Date();
+
+
+        double soma = 10.5 + 15.7;
+        int subtração = 113 - 25;
+        int multiplicacao = 20 * 7;
+        int divisao = 15 / 3;
+        int modulo = 18 % 3;
+        double resultado = (10 * 7) + (20/4);
+
     }
 }
